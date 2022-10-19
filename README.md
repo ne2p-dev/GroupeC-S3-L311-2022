6fa6c49da795d09b8ed6a47776a407a0f59ab15c
Nicolas Perrier <nicolas.perrier@etu.unilim.fr>
2022-10-19 13:30:33 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+## Sujet pour debug Front end UE L311 - Semaine 3 ## 
---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+var $jours = $('.jour');var $puces = $('.bullets .entypo-record		');
+
+					function init(){
+		setTimeout(function(){
+			$('body').addClass('isok');
+			$jours.hide();
+			$('.wrapper').fadeIn('slow', function(){
+				$jours.first().fadeIn('slow');
+				$puces.removeClass('active').first.addClass('active');
+			});
+		}, 2000);
+		
+	}$puce.click(function(){
+		var $this = $(this);var cible = $this.attr('data-cible');
+				$jours.hide();
+
+		$($jours.get(cible)).fadeIn()
+		$puces.removeClass('active'); $this.addClass('active');
+	});
+	init(); 
+);  
