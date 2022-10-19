4e746d1509e5d2f11f09f6847b5a3e6566fd8590
Nicolas Perrier <nicolas.perrier@etu.unilim.fr>
2022-10-19 15:38:57 +0000

    :bug:/:recycle:/:bulb: debug, refactor ans comment script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,31 @@
 $(document).ready(function(){
-var $jours = $('.jour');var $puces = $('.bullets .entypo-record		');
 
-					function init(){
-		setTimeout(function(){
-			$('body').addClass('isok');
-			$jours.hide();
-			$('.wrapper').fadeIn('slow', function(){
-				$jours.first().fadeIn('slow');
-				$puces.removeClass('active').first.addClass('active');
-			});
-		}, 2000);
-		
-	}$puce.click(function(){
-		var $this = $(this);var cible = $this.attr('data-cible');
-				$jours.hide();
+/* variable declaration */
+var jours = $('.jour');
+var puces = $('.bullets .entypo-record');
 
-		$($jours.get(cible)).fadeIn()
-		$puces.removeClass('active'); $this.addClass('active');
-	});
-	init(); 
-);  
+/* function init declaration */
+function init(){
+	setTimeout(function(){
+		$('body').addClass('isok');
+		jours.hide();
+		$('.wrapper').fadeIn('slow', function(){
+			jours.first().fadeIn('slow');
+			puces.removeClass('active').first.addClass('active');
+		});
+	}, 2000);
+}
+
+/* bullets click event */
+puces.click(function(){
+	var cible = $(this).attr('data-cible');
+	jours.hide();
+	$(jours.get(cible)).fadeIn();
+	puces.removeClass('active');
+	this.addClass('active');
+});
+
+/* function init */
+init();
+
+});  
