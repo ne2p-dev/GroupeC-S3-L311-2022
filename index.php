d535f59070199f126db73b8ab726dcc96ba4509a
lecomte38 <nicolas.lecomte@etu.unilim.fr>
2022-10-19 14:24:03 +0000

    :bug: debug html

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<title>Météo</title>
 	<link rel="stylesheet" href="assets/css/import.css" />
 	<link rel="stylesheet" href="assets/css/reset.css" />
-	<link rel="stylesheet" href="assets/css/preloader.csss" />
+	<link rel="stylesheet" href="assets/css/preloader.css" />
 	<link rel="stylesheet" href="assets/css/style.css" />
 </head>
 <body>
